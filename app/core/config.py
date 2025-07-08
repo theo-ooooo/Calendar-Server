dc6ec4a104ba794117c4f85d9b8c508da6f5619b
theo-ooooo <kkw.theo@gmail.com>
2025-07-08 14:23:51 +0900

    feat: Alembic 초기화 및 기본 설정

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
